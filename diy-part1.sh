fb3d18dc1aeea0216a07638d4eaa26083989db72
lin-xh <58517822+lin-xh@users.noreply.github.com>
2021-03-14 10:23:05 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,4 +15,4 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git koolproxyr https://github.com/cnzd/luci-app-koolproxyR' feeds.conf.default
+git clone https://github.com/cnzd/luci-app-koolproxyR package/luci-app-koolproxyR
